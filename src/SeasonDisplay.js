218719465984a238289850e958249f2853ddf937
Iza Habjan <iza.habjan@gmail.com>
2021-12-12 14:51:17 +0100

    icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -11,10 +11,13 @@ const SeasonDisplay = (props) => {
 
   const text =
     season === "winter" ? "Burr, it is chilly 🥶" : "Let's hit the beach 😎";
+  const icon = season === "winter" ? "snowflake" : "sun";
 
   return (
     <div>
+      <i className={`${icon} icon`} />
       <h1>{text}</h1>
+      <i className={`${icon} icon`} />
     </div>
   );
 };
